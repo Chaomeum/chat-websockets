8801a4b8cbea21d9f9993b7dfb12ccab67bdbe77
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-11-05 12:25:12 -0500

    first commit

---
+++ src/test/java/pe/edu/upc/jameofit/JameoFitApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.jameofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JameoFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
